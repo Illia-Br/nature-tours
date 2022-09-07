a717f06cb00368653d07f1579c49abcec7b6fa1d
Illia Brytun <elbrytun@gmail.com>
2022-09-07 14:03:35 -0400

    add eventlistener to close popup window

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ const checkbox = document.querySelector("#navi-toggle");
 navLinks.forEach(link => {
   link.addEventListener('click', () => checkbox.click())
 });
+
+//Close popup
+const popupClose = document.querySelector(".popup__close");
+
+document.addEventListener('click', (e) => {
+  if (e.target.id === 'popup') {
+    popupClose.click();
+  }
+})
